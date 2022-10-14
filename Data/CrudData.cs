2d1d5136662261cafce126322b8e5d5297d67081
Jonas Østergaard <jonasoe@hotmail.com>
2022-10-14 10:41:16 +0200

    working

--- Data/CrudData.cs
+++ Data/CrudData.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace BlazorCrud.Data
 {
@@ -11,48 +12,21 @@ namespace BlazorCrud.Data
 
 
 
-        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=UHDKDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        private SqlConnection conn = new SqlConnection("" + "Data Source=.;Initial Catalog=UHDKDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
 
-        public CrudData(string? weaponRank, string? weaponName, string? dropLocation, string? ilvl)
+        public bool CreateWeapon(content cont)
         {
-            this.weaponRank = Convert.ToInt32(weaponRank);
-            this.weaponName = weaponName;
-            this.dropLocation = dropLocation;
-            this.ilvl = Convert.ToInt32(ilvl);
-        }
-
-        public string CreateUser()
-        {
-
-            conn.Open();
-
-            SqlCommand command = new SqlCommand("SELECT * FROM WeaponsP1", conn);
-
-
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    if (weaponRank == reader.GetInt32(0) || weaponName == reader.GetString(1) || dropLocation == reader.GetString(2) || ilvl == reader.GetInt32(3))
-                    {
-                        return "";
-                    }
-                }
-            }
-
-            using (SqlDataAdapter adapter = new SqlDataAdapter())
-            {
-                adapter.InsertCommand = new SqlCommand("INSERT INTO WeaponsP1 VALUES (@weaponRank,@weaponName,@dropLocation,@ilvl)", conn);
-                adapter.InsertCommand.Parameters.AddWithValue("@weaponRank", weaponRank);
-                adapter.InsertCommand.Parameters.AddWithValue("@weaponName", weaponName);
-                adapter.InsertCommand.Parameters.AddWithValue("@dropLocation", dropLocation);
-                adapter.InsertCommand.Parameters.AddWithValue("@ilvl", ilvl);
-                adapter.InsertCommand.ExecuteNonQuery();
-
-            }
-
-            return "objects added to db";
+                bool executed = false;
+                conn.Open();
+                var cmd = new SqlCommand("INSERT INTO WeaponsP1 " + "VALUES (@weaponRank, @weaponName, @dropLocation, @ilvl)", conn);
+                cmd.Parameters.Add("@weaponRank", SqlDbType.Int).Value = cont.weaponRank;
+                cmd.Parameters.Add("@weaponName", SqlDbType.NVarChar).Value = cont.weaponName;
+                cmd.Parameters.Add("@dropLocation", SqlDbType.NVarChar).Value = cont.dropLocation;
+                cmd.Parameters.Add("@ilvl", SqlDbType.Int).Value = cont.ilvl;
+                if (cmd.ExecuteNonQuery() == 1) executed = true;
+                conn.Close();
+                return executed;
         }
         public List<content> ReadContent()
         {
@@ -64,14 +38,14 @@ namespace BlazorCrud.Data
 
                 while (reader.Read())
                 {
-                    content f = new()
+                    content c = new()
                     {
                         weaponRank = reader.GetInt32(0),
                         weaponName = reader.GetString(1),
                         dropLocation = reader.GetString(2),
                         ilvl = reader.GetInt32(3)
                     };
-                    contentList.Add(f);
+                    contentList.Add(c);
                 }
             }
             conn.Close();
